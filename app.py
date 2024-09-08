56f55c7c6872bcb2d259f26727fa3b4ecbdddaba
Matheus Audibert <matheusaudibert2019@outlook.com>
2024-09-08 00:56:20 -0300

    commit

--- app.py
+++ app.py
@@ -71,7 +71,7 @@ def main():
     with col3:
         display_projects(st, "#", top_30, start_index=21)
 
-    st.markdown("## ⏰ Próxima atualização às 00:00.")
+    st.markdown("## ⏰ Próxima atualização às 00:00. Total de projetos: 1,677")
     st.markdown(":orange[⭐ Deixe o seu like no meu projeto [aqui](https://discord.com/channels/1277631721822748742/1277631722716008535/1281647648096518155)]")
     st.markdown(":blue[🌐 Acesse o meu projeto [aqui](https://devspaceee.vercel.app/index.html)]")
     st.markdown("Repositório do ranking [aqui](https://github.com/matheusaudibert/ranking_alura)")
