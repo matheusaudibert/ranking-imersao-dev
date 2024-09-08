e9a567fb1e0aa69ec7363fe6bac4c7b6be21f2b4
Matheus Audibert <matheusaudibert2019@outlook.com>
2024-09-08 00:40:53 -0300

    commit

--- app.py
+++ app.py
@@ -70,7 +70,7 @@ def main():
         display_projects(st, "#", top_30, start_index=21)
 
     st.markdown("## Próxima atualização às 00:00.")
-    st.markdown("Repositório do meu projeto [aqui](https://github.com/matheusaudibert/devspace).")
+    st.markdown(":rainbow[Deixe o seu like no meu projeto! [DEVSPACE]](https://devspaceee.vercel.app/index.html)")
     st.markdown("Repositório do ranking [aqui](https://github.com/matheusaudibert/ranking_alura).")
 
 if __name__ == "__main__":
